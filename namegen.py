c62e0630b1885806365c20faf8fe3fb145a8addc
Sam Himes <samhimes@gmail.com>
2018-10-26 18:43:13 -0400

    Fix bugs, add directional generation, and more
    Fix "--start" raising exceptions in both of the namegen scripts.
    Add a "direction" flag to add more customization to name generation, and fix unexpected behaviors when using "--start" or when "None" appears in a term early on.
    Strip trailing whitespace.
    Mush the letters together of syllabified names as well as the string of syllables used to construct it.
    Case insensitivity for --start and elements in the chain generator (normalizing on x.lower())

--- namegen.py
+++ namegen.py
@@ -4,70 +4,70 @@ import sys, random, argparse
 
 from namegen_utils import *
 
-# -------------------------------------------------------------------------------------------------	
+# -------------------------------------------------------------------------------------------------
 # Split entries by rule
 # -------------------------------------------------------------------------------------------------
 def PartitionGroup(entries, whatfunc, position):
 
 	'''
-	
+
 	Splits apart each of entries into a list of chunks based on `whatfunc`.
-	
+
 	`whatfunc` is expected to return a tuple of two objects:
 		matches, consumed = whatfunc(substr)
-		
+
 		When `matches` is True, it indicates the word needs to be split in
 		its current position.  Everything stored before this term will be:
-		
+
 			1) `position`=='after'  : Appended to the current collection of non-matching letters
 				'food', split after 'oo', becomes ['foo', 'd']
-			
+
 			2) `position`=='before' : Appended at the start of the next collection of non-matching letters
 				'food', split before 'oo', becomes ['f', 'ood']
-				
+
 			3) `position`=='around' : Appended as a unique group after the current non-matching letter group.
 				'food', split around 'oo', becomes ['f', 'oo', 'd']
-				
+
 			4) `position`=='random' : Nondeterministic.  Randomly picks 'before'/'after'/'around' after each
 			    successful match or iteration performed.
-			
+
 	'''
-	
+
 	original_position = position
-		
+
 	for word in entries:
-	
+
 		letterlist  = list(word)
 		nonmatching = ['']
-		
+
 		while letterlist:
-		
+
 			if original_position == 'random':
 				position = random.choice(('before', 'after', 'around'))
-		
+
 			match, consume = whatfunc(letterlist)
-			
+
 			if match:
-			
+
 				consumed   = ''.join(letterlist[:consume])
 				letterlist = letterlist[consume:]
-				
+
 				if position == 'before':
 					nonmatching.append(consumed)
-					
+
 				elif position == 'after':
 					nonmatching[-1] += consumed
 					nonmatching.append('')
-					
+
 				else:
 					nonmatching.extend([consumed, ''])
-				
+
 			else:
 				nonmatching[-1] += letterlist.pop(0)
-							
+
 		yield tuple(x for x in nonmatching if x)
-		
-		
+
+
 # -------------------------------------------------------------------------------------------------
 #
 # Various rule lists for splitting up words.
@@ -77,40 +77,40 @@ def PartitionGroup(entries, whatfunc, position):
 def letters(what):
 	''' Return the very first letter in the term, and indicate we consume a single letter. '''
 	return True, 1
-	
+
 def eachvowel(what):
 	''' Return True only if we're a vowel, and consume a single letter. '''
 	return what[0] in VOWEL_SET, 1
-	
+
 def groupedvowels(what):
 	''' Consume all substrings of vowels length 2 or larger. '''
-	
+
 	count = 0
 	for elem in what:
 		if elem in VOWEL_SET:
 			count += 1
 		else:
 			break
-			
+
 	if count >= 2:
 		return True, count
 	return False, 1
-	
+
 
 def twocommon(what):
-		
+
 	''' Consume two letters and return True if two common letters are next to each other. '''
 	if len(what) >= 2 and all(x in COMMON_LETTERS for x in what[:2]):
 		return True, 2
-		
+
 	return False, 1
-	
-	
+
+
 def choose_randomly(what):
 	''' Nondeterministic.  Pick a random method from METHOD_MAPPING.'''
 	return random.choice(list(METHOD_MAPPING.items()))[1](what)
-	
-	
+
+
 METHOD_MAPPING = {
 	'letters'         : letters,
 	'eachvowel'       : eachvowel,
@@ -118,8 +118,8 @@ METHOD_MAPPING = {
 	'twocommon'       : twocommon,
 	'choose_randomly' : choose_randomly,
 }
-	
-					
+
+
 # -------------------------------------------------------------------------------------------------
 # Drive the program fooooooorward into the future!
 # -------------------------------------------------------------------------------------------------
@@ -129,46 +129,49 @@ if __name__ == '__main__':
 
 	ap.add_argument('--minlen', type=int, default=4,  help='Minimum string length of generated names.')
 	ap.add_argument('--maxlen', type=int, default=13, help='Force markov chain termination if the name is at least this size.')
-	
+
+	ap.add_argument('-d', '--direction', default='forward',
+		choices = ('forward', 'backward', 'bidirectional'),
+		help    = 'When generating names, specify if we generate forward (at the end of the word), backward, or in both directions randomly.')
+
 	ap.add_argument('-s', '--start', nargs='+',
 		help='A series of letter or letters names must start with.')
-	
+
 	ap.add_argument('-i', '--input', required=True, nargs='+',
 		help='Input file(s) containing a list of names.  Each file must have one word/name per line.')
-	
+
 	ap.add_argument('--method', default='letters',
 		choices = set(METHOD_MAPPING),
 		help    = 'Determine how letters in words will be split apart before the probability list is constructed.')
-		
+
 	ap.add_argument('--split', default='around',
 		choices = ('around', 'before', 'after', 'random'),
 		help    = 'When splitting a word apart (specified via -m/--method), determine how to break apart the word at a given separation point.')
 
 	args = ap.parse_args()
-		
+
 	if args.minlen >= args.maxlen or args.minlen < 1 or args.maxlen < 1:
 		raise ValueError('The --minlen and --maxlen parameters must be larger than zero and a valid increasing range from minlen to maxlen')
-		
+
 	args.method = METHOD_MAPPING[args.method]
-		
+
 	entries = [x.lower() for x in YieldNames(args.input)]
-	
+
 	chain = MarkovChainHandler(args)
 	for entry in PartitionGroup(entries, args.method, args.split):
 		chain.UpdateTermString(entry)
-			
+
 	seen = set(entries)
-	
+
 	while 1:
 		generated = chain.GenerateChain()
 		if generated:
 			stringified = ''.join(generated)
 			if stringified not in seen:
 				seen.add(stringified)
-				
+
 				stringified = stringified.capitalize()
 				aligned     = '{:<' + str(args.maxlen + 1) + '}'
 				show_name   = '{} =>'.format(aligned.format(stringified))
-					
+
 				input(show_name)
-					
--- namegen_utils.py
+++ namegen_utils.py
@@ -14,96 +14,96 @@ def Chunk(l, s):
 	for i in range(0, len(l), s):
 		yield l[i:i+s]
 
-		
+
 def RemoveNumbers(word):
 	'''Remove numbers from a word.'''
 	return ''.join(letter for letter in word if letter not in NUMBERS)
-	
-	
+
+
 def FilterLetters(word):
 	'''Strip all elements from the input word that aren't in VALID_LETTER_SET.'''
 	return ''.join(letter for letter in word if letter in VALID_LETTER_SET)
-			
-			
+
+
 def ValidLines(fname):
 	'''
 	Returns all lines in a file that contain letters.
 	All trailing whitespace is stripped from the entry.
 	'''
-	
+
 	with open(fname, 'r') as f:
 		for each_line in f:
 			stripped = each_line.rstrip()
 			if stripped:
 				yield stripped
 
-			
+
 def ChunkSyllables(syllables):
 	'''
 	Group an ordered list of syllables into chunks of syllables, such that a raw list of [a, b, c] will yield the following results:
 		1) [[a, b, c]]
 		2) [[a], [b, c]]
-		3) [[a, b], [c]]	
+		3) [[a, b], [c]]
 	'''
-	
+
 	if syllables:
-	
+
 		# Handles "[[a, b, c]]"
 		yield [syllables]
-	
+
 		# Handles "[[a], [b, c]]" and "[[a, b], [c]]"
 		for j in range(1, len(syllables)):
 			me   = syllables[:j]
 			rest = syllables[j:]
-			
+
 			for result in ChunkSyllables(rest):
 				yield [me] + list(result)
 
-				
+
 def YieldAllFrom(*args):
 	'''Given an arbitrary arglist of iterables, yield each entry for all iterables.'''
 	for arg in args:
 		yield from arg
-			
+
 
 def YieldNames(files):
 	'''
 	Collects and returns a list of every non-empty line read in the specified input files.
 	Ignores lines considered SYLLABLES (which are denoted by leading whitespace).
-	'''	
+	'''
 	for f in files:
 		for line in ValidLines(f):
 			if line.strip() == line:
 				yield line
-				
-				
+
+
 def ObtainSyllables(files, includeEmpty=False):
 	'''
 	Captures all names that have syllable definitions.
-	
+
 	IncludeEmpty is a utility that returns all entries, even if
 	there aren't any syllables associated with them.  This can be
 	useful if we want to generate syllables for these words.
-	'''	
-	
+	'''
+
 	words = defaultdict(set)
-	
+
 	for f in files:
-	
+
 		lastWord = None
 		for line in ValidLines(f):
-		
+
 			if line.strip() == line:
 				lastWord = line
 				if includeEmpty:
 					words[lastWord] = set()
-				
-			else:		
+
+			else:
 				words[lastWord].add(tuple(line.strip().split(' ')))
-				
+
 	return words
-		
-	
+
+
 class Transitions(object):
 
 	'''
@@ -114,198 +114,207 @@ class Transitions(object):
 	__slots__ = ('_value', '_sources', '_transitionCache', '_prepared')
 
 	def __init__(self, val):
-	
+
 		self._value = val
-		
+
 		self._sources = {
 			'to'   : defaultdict(int),
 			'from' : defaultdict(int),
 		}
-		
+
 		# Cascades is a weird construct without context -- the 'True' and 'False'
 		# indicate whether it's allowed to transition to a "None" key or not.
-		self._transitionCache = { 
+		self._transitionCache = {
 			True  : {},
 			False : {},
 		}
-		
+
 		self._prepared = False
-	
-	
+
+
 	def ConnectWith(self, whatterm, direction):
-	
+
 		'''
 		Connect this chain element with another element either before or after it.
-		
+
 		Directions must be the strings 'to' or 'from' for the _GenerateCache cache
 		to be generated properly.
 		'''
-		
+
 		self._sources[direction][whatterm] += 1
 		self._prepared = False
-		
-		
+
+
 	def _GenerateCache(self):
-	
+
 		'''
 		This effectively creates a list representing the states we can transition to and from.
-		
+
 		This perfectly represents the number of times we move to an arbitrary instance (if we move from this
 		object to the letter 'a' 10 times, the entry 'a' will be present in the list 10 times).
-		
+
 		While this keeps code simple in the trivial case (random.choice(list)), this obviously can cause
 		problems with memory if the number of recorded transitions between elements are very large.
 		'''
-		
+
 		if not self._prepared:
-		
+
 			# Iterate through to and from terms
 			for direction, transitions in self._sources.items():
-		
+
 				# Generate dictionaries for ignoring and listening for "None" transitions.
 				# The "None" transition indicates that we're at the end of a word (if transitioning 'to'),
 				# or at the very start (if transitioning 'from').
 				for noNones in [True, False]:
-				
+
 					tmp_transition = {}
 					total = 0
-		
+
 					for term, value in transitions.items():
-					
+
 						# If we don't want 'None' terms and this is a 'None', ignore it.
 						if noNones and term is None:
 							continue
-								
+
 						for j in range(total, total+value):
 							tmp_transition[j] = term
-					
+
 						total += value
-				
+
 					self._transitionCache[noNones][direction] = tmp_transition
-				
+
 			self._prepared = True
 
-			
+
 	def PickRandomTerm(self, direction, noNones=False):
-	
+
 		'''
 		Randomly pick an element connecting to this element in the desired direction ('to' or 'from).
 		If 'noNones' is true, it will only return None if this term doesn't connect to anything else in the specified direction.
 		'''
 
 		self._GenerateCache()
-		
-		transitions = self._transitionCache[noNones][direction]		
+
+		transitions = self._transitionCache[noNones][direction]
 		if not transitions:
 			return None
-			
+
 		return random.choice(transitions)
-		
-		
+
+
 	def __str__(self):
-	
+
 		return '\n'.join([
-			self.value, 
+			self.value,
 			'\tTo:  \n\t\t{}'.format(self._sources['to']),
 			'\tFrom:\n\t\t{}'.format(self._sources['from']),
 		])
-		
-		
-# -------------------------------------------------------------------------------------------------		
+
+
+# -------------------------------------------------------------------------------------------------
 # MarkovChainHandler
-# -------------------------------------------------------------------------------------------------		
+# -------------------------------------------------------------------------------------------------
 class MarkovChainHandler(object):
 
 	def __init__(self, params):
-		
+
 		self._connections = { None : Transitions(None) }
 		self._args = params
-		
-		
+
+
 	def Debug(self):
 		''' Show all connections that the MarkovChainHandler has registered. '''
 		for name, obj in self._connections.items():
 			print(f'For object called "{name}": \n{obj}')
-	
-	
+
+
 	def UpdateTermString(self, terms):
-	
+
 		''' Given an iterable sequence of terms, bidirectionally connect elements with their neighbors. '''
-		
+
 		# Ensure all terms exist. ---------------------------------------------
+
+		terms = [x.lower() for x in terms]
 		
 		for term in terms:
 			if term not in self._connections:
 				self._connections[term] = Transitions(term)
-				
+
 		# Connect neighboring terms with each other. --------------------------
-		
-		for thisTerm, nextTerm in zip(terms[:-1], terms[1:]):	
-		
+
+		for thisTerm, nextTerm in zip(terms[:-1], terms[1:]):
+
 			self._connections[thisTerm].ConnectWith(nextTerm, 'to')
 			self._connections[nextTerm].ConnectWith(thisTerm, 'from')
-			
+
 		# Connect the starting and ending term with 'none.' -------------------
-		
+
 		self._connections[None].ConnectWith(terms[0], 'to')
 		self._connections[terms[0]].ConnectWith(None, 'from')
-		
+
 		self._connections[terms[-1]].ConnectWith(None, 'to')
 		self._connections[None].ConnectWith(terms[-1], 'from')
-				
-		
+
+
 	def GenerateChain(self):
-	
+
 		'''
 		Generates a sequence of elements based on input arguments specified in the constructor.
-		
-		Returns either `None` or a sequence of elements 
+
+		Returns either `None` or a sequence of elements
 		'''
-	
+
 		chain = deque()
-		
+
 		# By default, start using "None" to force generations to start with terms
 		# explicitly used in the input. This can be overridden by the args constructed
 		# with this class.
-		
+
 		starting_term = None
+		arg_start_terms = [x.lower() for x in self._args.start]
 		if self._args.start:
-		
+
 			# Check if any of the starting terms are valid.
-			if not any(term in self._connections for term in self._args.start):
+			if not any(term in self._connections for term in arg_start_terms):
 				raise ValueError(f'None of the starting terms ({self._args.start}) exist as elements in the Markov Chain handler!')
-			
-			valid_starting_terms = set(self._connections) & set(self._args.start)
-			starting_term = random.choice(valid_starting_terms)
-			
-		chain.append(self._connections[starting_term].PickRandomTerm('to', True))
-		
+
+			valid_starting_terms = set(self._connections) & set(arg_start_terms)
+			starting_term = random.choice(list(valid_starting_terms))
+			chain.append((starting_term))
+
+		else:
+			chain.append(self._connections[starting_term].PickRandomTerm('to', True))
+
 		if chain[0] is None:
 			return None
 
-		# -----------------------------------------------------------------------------			
+		# -----------------------------------------------------------------------------
 		# Generate the name until we think it's good.
-		# -----------------------------------------------------------------------------	
-		
+		# -----------------------------------------------------------------------------
+
 		while 1:
-							
-			if chain[-1] is None:
-				nextTerm = self._connections[chain[0]].PickRandomTerm('from', len(chain) < 3)
-				chain.appendleft(nextTerm)
-			else:
-				nextTerm = self._connections[chain[-1]].PickRandomTerm('to', len(chain) < 3)
-				chain.append(nextTerm)
-					
+
 			stringified = ''.join(x for x in chain if x)
-					
-			if chain[-1] is None and len(stringified) < self._args.minlen:
-				return None
-					
-			if chain[-1] is None and chain[0] is None:
+			if len(stringified) >= self._args.maxlen:
 				break
-				
-			elif len(stringified) >= self._args.maxlen:
-				break
-	
+
+			allowed_directions = []
+			if chain[0]  is not None and self._args.direction != 'forward':  allowed_directions.append('backward')
+			if chain[-1] is not None and self._args.direction != 'backward': allowed_directions.append('forward')
+
+			if not allowed_directions:
+				if len(stringified) < self._args.minlen:
+					return None
+				return stringified
+
+			temp_direction = random.choice(allowed_directions)
+
+			if temp_direction == 'forward':
+				nextTerm = self._connections[chain[-1]].PickRandomTerm('to', len(chain) < self._args.minlen)
+				chain.append(nextTerm)
+			else:
+				nextTerm = self._connections[chain[0]].PickRandomTerm('from', len(chain) < self._args.minlen)
+				chain.appendleft(nextTerm)
+
 		return tuple(x for x in chain if x)
-					
+
